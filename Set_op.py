dbfa4dcac1b2846fece3edbf0b14a5ee30df51fb
Deepak Chandhru N <122144025+Deepakchandhru@users.noreply.github.com>
2024-06-15 10:49:06 +0530

    Delete Set_op.py

--- Set_op.py
+++
@@ -1,6 +0,0 @@
-E=set(map(int,input("Enter numbers").split(",")))
-N=set(map(int,input("Enter numbers").split(",")))
-print("Union of E and N is",E|N)
-print("Intersection of E and N is",E&N)
-print("Difference of E and N is",E-N)
-print("Symmetric difference of E and N is",E^N)
